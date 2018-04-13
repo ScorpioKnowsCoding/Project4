ff15da5e70a0ab001a549627ec86983b5bef092b
Sara Gaya <sgaya16@ufl.edu>
2018-04-13 11:03:00 -0400

    P4 6740.1 edited

--- StudentAttackerController.java
+++ StudentAttackerController.java
@@ -20,6 +20,7 @@ public final class StudentAttackerController implements AttackerController
 		int pathDistance;
 		Node target;
 		Node targetGhosts;
+		Node defenderLocation;
 		Node gatorLocation = game.getAttacker().getLocation(); //gets location of the gator
 		Attacker gator = game.getAttacker(); //gets the attacker (the gator)
 		List<Defender> defenders = game.getDefenders(); //list of the 4 defenders in the game
@@ -30,22 +31,23 @@ public final class StudentAttackerController implements AttackerController
 		target = gator.getTargetNode(pillList, true); //sets target to go after the pill list
 		action = gator.getNextDir(target, true); //gator goes after pills
 
+
 		for (i = 0; i < 4; ++i) {
 			if (defenders.get(i).getLocation() == defenderStartLocations) { //if defender's are in the lair (starting location)
 				target = gator.getTargetNode(pillList, true); //gator goes after pill list
 				action = gator.getNextDir(target, true); //gator goes after pill list
 			}
-			if (!defenders.get(i).isVulnerable()) { //if defenders are not vulnerable
-				Node defenderLocation = defenders.get(i).getLocation();
-				pathDistance = gatorLocation.getPathDistance(defenderLocation); //finds distance b/w gator and ghosts
-				if (pathDistance < 5) { //if distance is less than 5 nodes away
-					action = gator.getNextDir(defenderLocation, false); //gator runs away from defender
-				}
-			}
 			if (defenders.get(i).isVulnerable()) { //if defenders are vulnerable
 				ghostTargets.add(defenders.get(i).getLocation());
 				targetGhosts = gator.getTargetNode(ghostTargets, true ); //finds closest ghost
 				action = gator.getNextDir(targetGhosts, true); //gator goes after the closest vulnerable ghost
+			}
+			else { //else if defenders are not vulnerable
+				defenderLocation = defenders.get(i).getLocation();
+				pathDistance = gatorLocation.getPathDistance(defenderLocation); //finds distance b/w gator and ghosts
+				if (pathDistance < 5) { //if distance is less than 5 nodes away
+					action = gator.getNextDir(defenderLocation, false); //gator runs away from defender
+				}
 			}
 		}
 
